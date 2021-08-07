a55c90cfc502fb9205b31b1bc8e5799abee440df
Mary <mgansall@terpmail.umd.edu>
2021-08-07 21:23:30 +0000

    Initial commit

---
+++ static/moonclerk.js
@@ -0,0 +1,16 @@
+var mc1x9x8gl1q92p
+;(function (d, t) {
+  var s = d.createElement(t),
+    opts = { checkoutToken: "1x9x8gl1q92p", width: "100%" }
+  s.src = "https://d2l7e0y6ygya2s.cloudfront.net/assets/embed.js"
+  s.onload = s.onreadystatechange = function () {
+    var rs = this.readyState
+    if (rs) if (rs != "complete") if (rs != "loaded") return
+    try {
+      mc1x9x8gl1q92p = new MoonclerkEmbed(opts)
+      mc1x9x8gl1q92p.display()
+    } catch (e) {}
+  }
+  var scr = d.getElementsByTagName(t)[0]
+  scr.parentNode.insertBefore(s, scr)
+})(document, "script")
